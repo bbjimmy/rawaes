87f737fbfb0e970d8c66ba35207f35e6819f8de7
Jim <black.belt.jimmy@gmail.com>
2015-01-23 14:32:39 -0800

    update makefile update readme, added license text.

---
+++ objects.x86-gcc2-release/aes.d
@@ -0,0 +1,4 @@
+# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
+objects.x86-gcc2-release/aes.o : aes/aes_bet.h aes/aes_let.h aes/aes.h
+
+# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
